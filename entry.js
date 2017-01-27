bddd660117f0e91acd3f9693c97450d95fe4e403
Rose Gauthier <rgauthier@rgauthier.local>
2017-01-26 21:08:06 -0500

    inital commit

---
+++ entry.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Include your React components like this:
+// import MyComponent from 'components/my_component';
+
+ReactDOM.render(<div>React Element goes here</div>, document.getElementById("placeholder"));
