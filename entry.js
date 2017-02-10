8ec959f41021f13b52dd23163d44425406d6f151
Maureen Holland <maureen.holland11@gmail.com>
2017-02-09 21:08:30 -0500

    Add database

--- entry.js
+++ entry.js
@@ -8,6 +8,17 @@ import Login from './components/login';
 import ManageDatasets from './components/manage_datasets';
 import TrackData from './components/track_data';
 
+import firebase from 'firebase';
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC03jOtrLV6FF2VTSw1h1Igx6EBhT2NToE",
+    authDomain: "tracker-12281.firebaseapp.com",
+    databaseURL: "https://tracker-12281.firebaseio.com",
+    storageBucket: "tracker-12281.appspot.com",
+    messagingSenderId: "802767118279"
+  };
+  firebase.initializeApp(config);
+
 ReactDOM.render(<Router history={ browserHistory }>
 					<Route path="/" component={ App }>
 						<IndexRoute component={ Login } />
