7b930923e2189d560338bf6fe7ae7e17e722b800
Maureen Holland <maureen.holland11@gmail.com>
2017-02-16 14:44:33 -0500

    Styles for track data component

--- components/track_data.js
+++ components/track_data.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
-import { Navbar, NavItem } from 'react-materialize';
+import { Row, Col, Button, Card, Navbar, NavItem, Form, Input } from 'react-materialize';
 
 import Header from './nav_bar'
 
@@ -10,41 +10,41 @@ class TrackData extends React.Component {
 		return (
 			<main>
 				<Header />
-				<section>
-					<form action="">
-						<label htmlFor="category">
-							Category
-							<select name="category" id="category">
-								<option value="groceries">Groceries</option>
-								<option value="rent">Rent</option>
-								<option value="transit">Transit</option>
-								<option value="phone">Phone</option>
-							</select>
-						</label>
-						<label htmlFor="vendor">
-							Vendor
-							<input type="text" id="vendor" />
-						</label>
-						<label htmlFor="amount"> 
-							Amount
-							<input type="text" id="amount" />
-						</label>
-						<label htmlFor="date">
-							Date
-							<input type="date" id="date" />
-						</label>
-						<input type="submit" id="date" />
-					</form>
-				</section>
-				<section>
-					<ul className="tablist" role="tablist">
-						<li role="tab">Bar</li>
-						<li role="tab">Pie</li>
-						<li role="tab">Line</li>
-						<li role="tab">Table</li>
-					</ul>
-					<div role="tabpanel"></div>
-				</section>
+				<Row>
+					<Col s={4}>
+						<Card className="white">
+							<Row>
+								<Input s={12} defaultValue="rent" label="Category" type="select" name="category" id="category">
+									<option value="groceries">Groceries</option>
+									<option value="rent">Rent</option>
+									<option value="transit">Transit</option>
+									<option value="phone">Phone</option>
+								</Input>
+								<Input s={12} label="Vendor" type="text" id="vendor" />
+								<Input s={12} label="Amount" type="text" id="amount" />							
+								<Input s={12} type="date" id="date" />
+								<Button className="submit dark-primary-color">Submit</Button>
+							</Row>
+						</Card>
+					</Col>
+					<Col s={8}>
+						<Card className="white">
+							<Navbar className="tablist right" role="tablist">
+								<NavItem role="tab">Bar</NavItem>
+								<NavItem role="tab">Pie</NavItem>
+								<NavItem role="tab">Line</NavItem>
+								<NavItem role="tab">Table</NavItem>
+							</Navbar>
+							<div role="tabpanel"></div>
+							<Button id="date" className="submit dark-primary-color">Compare</Button>
+							<label>
+								Show Target Dataset
+								<Input name='on' type='switch' value='1' />
+							</label>
+							
+						</Card>
+					</Col>
+				</Row>
 			</main>
 		)
 	}
